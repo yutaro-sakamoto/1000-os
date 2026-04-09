cf2cf590cb20a085a7caf550677d09e82fd8287f
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-04-09 20:36:29 +0900

    feat: launch the simple kernel

--- run.sh
+++ run.sh
@@ -2,5 +2,9 @@
 set -xue
 
 QEMU=qemu-system-riscv32
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
-${QEMU} -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+${CC} ${CFLAGS} -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+
+timeout --signal=KILL 5 ${QEMU} -machine virt -bios default -nographic -serial mon:stdio --no-reboot || true
