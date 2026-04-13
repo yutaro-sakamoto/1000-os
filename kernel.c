2b24300268bd4dd89baa792f471f697fbd55ecf5
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-04-13 19:57:41 +0900

    feat: implement some functions of standard libraries

--- kernel.c
+++ kernel.c
@@ -30,13 +30,6 @@ void putchar(char ch) {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
 }
 
-void *memset(void *buf, char c, size_t n) {
-    uint8_t *p = (uint8_t *) buf;
-    while (n--)
-        *p++ = c;
-    return buf;
-}
-
 void kernel_main(void) {
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
