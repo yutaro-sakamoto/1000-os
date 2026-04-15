b3fd7e39da6be7f889ba5b3749e055d6a727f610
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-04-15 21:01:50 +0900

    add: kernel_entry and handle_trap

--- run.sh
+++ run.sh
@@ -16,7 +16,8 @@ done
 
 QEMU=qemu-system-riscv32
 CC=clang
-WARNINGS="-Wall -Werror"
+#WARNINGS="-Wall -Werror"
+WARNINGS="-Wall"
 CFLAGS="-std=c11 -O2 -g3 ${WARNINGS} -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
 ${CC} ${CFLAGS} -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
