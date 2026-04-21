b154951d0d58656633f3d5ff39495e6709c4125c
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-04-21 21:05:42 +0900

    wip: wip

--- kernel.c
+++ kernel.c
@@ -294,3 +294,23 @@ void kernel_main(void) {
     yield();
     PANIC("unreachable here!");
 }
+
+void map_page(uint32_t *table1, uint32_t vaddr, paddr_t paddr, uint32_t flags) {
+    if(!is_aligned(vaddr, PAGE_SIZE)) {
+        PANIC("unaligned vaddr %x", vaddr);
+    }
+
+    if(!is_aligned(paddr, PAGE_SIZE)) {
+        PANIC("unaligned paddr %x", paddr)
+    }
+
+    uint32_t vpn1 = (vaddr >> 2) && 0x3ff;
+    if((table1[vpn1] & PAGE_V) == 0) {
+        uint32_t pt_paddr = alloc_pages(1);
+        table[vpn1] = ((pt_paddr / PAGE_SIZE) << 10) | PAGE_V;
+    }
+
+    uint32_t vpn0 = (vaddr >> 12) & 0x3ff;
+    uint32_t *table0 = (uint32_t *) ((table1[vpn1] >> 10) * PAGE_SIZE);
+    table0[vpn0] = ((paddr / PAGE_SIZE) << 10) | flags | PAGE_V;
+}
