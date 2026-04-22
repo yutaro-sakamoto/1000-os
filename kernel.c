0fbeac0670bce6d451ecac1e4aa20d02ce909320
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-04-22 23:25:26 +0900

    wip: wip

--- kernel.c
+++ kernel.c
@@ -185,6 +185,28 @@ __attribute__((naked)) void switch_context(uint32_t *prev_sp, uint32_t *next_sp)
     );
 }
 
+void map_page(uint32_t *table1, uint32_t vaddr, paddr_t paddr, uint32_t flags) {
+    if(!is_aligned(vaddr, PAGE_SIZE)) {
+        PANIC("unaligned vaddr %x", vaddr);
+    }
+
+    if(!is_aligned(paddr, PAGE_SIZE)) {
+        PANIC("unaligned paddr %x", paddr);
+    }
+
+    uint32_t vpn1 = (vaddr >> 22) & 0x3ff;
+    if((table1[vpn1] & PAGE_V) == 0) {
+        uint32_t pt_paddr = alloc_pages(1);
+        table1[vpn1] = ((pt_paddr / PAGE_SIZE) << 10) | PAGE_V;
+    }
+
+    uint32_t vpn0 = (vaddr >> 12) & 0x3ff;
+    uint32_t *table0 = (uint32_t *) ((table1[vpn1] >> 10) * PAGE_SIZE);
+    table0[vpn0] = ((paddr / PAGE_SIZE) << 10) | flags | PAGE_V;
+}
+
+extern char __kernel_base[];
+
 struct process *create_process(uint32_t pc) {
     struct process *proc = NULL;
     int i;
@@ -213,9 +235,17 @@ struct process *create_process(uint32_t pc) {
     *--sp = 0;                      // s0
     *--sp = (uint32_t) pc;          // ra
 
+    uint32_t* page_table = (uint32_t *)alloc_pages(1);
+
+    for(paddr_t paddr = (paddr_t) __kernel_base;
+        paddr < (paddr_t) __free_ram_end; paddr += PAGE_SIZE) {
+        map_page(page_table, paddr, paddr, PAGE_R | PAGE_W | PAGE_X);
+    }
+
     proc->pid = i + 1;
     proc->state = PROC_RUNNABLE;
     proc->sp = (uint32_t)sp;
+    proc->page_table = page_table;
     return proc;
 }
 
@@ -293,24 +323,4 @@ void kernel_main(void) {
 
     yield();
     PANIC("unreachable here!");
-}
-
-void map_page(uint32_t *table1, uint32_t vaddr, paddr_t paddr, uint32_t flags) {
-    if(!is_aligned(vaddr, PAGE_SIZE)) {
-        PANIC("unaligned vaddr %x", vaddr);
-    }
-
-    if(!is_aligned(paddr, PAGE_SIZE)) {
-        PANIC("unaligned paddr %x", paddr)
-    }
-
-    uint32_t vpn1 = (vaddr >> 2) && 0x3ff;
-    if((table1[vpn1] & PAGE_V) == 0) {
-        uint32_t pt_paddr = alloc_pages(1);
-        table[vpn1] = ((pt_paddr / PAGE_SIZE) << 10) | PAGE_V;
-    }
-
-    uint32_t vpn0 = (vaddr >> 12) & 0x3ff;
-    uint32_t *table0 = (uint32_t *) ((table1[vpn1] >> 10) * PAGE_SIZE);
-    table0[vpn0] = ((paddr / PAGE_SIZE) << 10) | flags | PAGE_V;
 }
--- kernel.h
+++ kernel.h
@@ -68,13 +68,14 @@ struct process {
     int pid;
     int state;
     vaddr_t sp;
+    uint32_t* page_table;
     uint8_t stack[8102];
 };
 
 struct process procs[PROCS_MAX];
 
 #define SATP_SV32 (1u << 31)
-#define PAVE_V (1 << 0)
+#define PAGE_V (1 << 0)
 #define PAGE_R (1 << 1)
 #define PAGE_W (1 << 2)
 #define PAGE_X (1 << 3)
