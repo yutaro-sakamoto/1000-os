ba5e80a32b9a13f668f0bd7fa61fd3a1eba2b55a
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-04-29 20:34:52 +0900

    wip: wip

--- run.sh
+++ run.sh
@@ -19,7 +19,12 @@ CC=clang
 #WARNINGS="-Wall -Werror"
 WARNINGS="-Wall"
 CFLAGS="-std=c11 -O2 -g3 ${WARNINGS} -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
+OBJCOPY=llvm-objcopy
 
-${CC} ${CFLAGS} -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
+${CC} ${CFLAGS} -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+${OBJCOPY} --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+${OBJCOPY} -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
+${CC} ${CFLAGS} -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c shell.bin.o
 
 ${TIMEOUT} ${QEMU} -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf || [ -n "${TIMEOUT}" ]
