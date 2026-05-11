12cd7ba684bcecf5d366ad9eebe4ce117faeb7c6
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-05-11 22:04:35 +0900

    wip: wip

--- kernel.c
+++ kernel.c
@@ -125,6 +125,16 @@ void kernel_entry(void) {
 
 void handle_syscall(struct trap_frame *f) {
     switch(f->a3) {
+        case SYS_GETCHAR:
+            while(1) {
+                long ch = getchar();
+                if(ch >= 0) {
+                    f->a0 = ch;
+                    break;
+                }
+                yield();
+            }
+            break;
         case SYS_PUTCHAR:
             putchar(f->a0);
             break;
