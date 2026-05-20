5c993dbb8233bb8c0eacccae5f463c03e351b7c8
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-05-20 20:44:49 +0900

    wip: wip

--- kernel.c
+++ kernel.c
@@ -376,6 +376,23 @@ __attribute__((naked)) void user_entry(void) {
     );
 }
 
+
+uint32_t virtio_reg_read32(unsigned offset) {
+    return *((volatile uint32_t *) (VIRTIO_BLK_PADDR + offset));
+}
+
+uint64_t virtio_reg_read64(unsigned offset) {
+    return *((volatile uint64_t *) (VIRTIO_BLK_PADDR + offset));
+}
+
+void virtio_reg_write32(unsigned offset, uint32_t value) {
+    *((volatile uint32_t *) (VIRTIO_BLK_PADDR + offset)) = value;
+}
+
+void virtio_reg_fetch_and_or32(unsigned offset, uint32_t value) {
+    virtio_reg_write32(offset, virtio_reg_read32(offset) | value);
+}
+
 void kernel_main(void) {
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
